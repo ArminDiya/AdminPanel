a683102e87f03246b2d201969466d7b331f2e16a
ArminDiya <diyaarmin@gmail.com>
2022-08-22 23:27:01 +0430

    Added Pages and APPs completely

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, pdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, pdfExport, Edit, Inject, PdfExport } from '@syncfusion/ej2-react-grids';
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../component';
 
@@ -7,12 +7,19 @@ const Orders = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
     <Header category='Page' title='Orders'/>
-    <GridComponent>
+    <GridComponent
+    id='gridcomp'
+    dataSource={ordersData}
+    allowPaging
+    allowSorting
+    >
       <ColumnsDirective>
         {ordersGrid.map((item, index) => (
           <ColumnDirective key={index} {...item}/>
+          
         ))}
       </ColumnsDirective>
+      <Inject services={[Resize, Sort, ContextMenu, Filter, Page, Edit, ExcelExport, PdfExport]}/>
     </GridComponent>
     </div>
   )

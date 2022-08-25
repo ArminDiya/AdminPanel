d94e7be219fa3deb0ce6ca09e68a2ae16a678b3f
ArminDiya <diyaarmin@gmail.com>
2022-08-25 14:03:40 +0430

    fixing stacked

--- src/component/Charts/Stacked.jsx
+++ src/component/Charts/Stacked.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip} from '@syncfusion/ej2-react-charts'
-import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis} from '../../data/dummy'
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis} from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 const Stacked = ({width, height}) => {
+  const { currentMode } = useStateContext();
   return (
     <ChartComponent 
     width={width}
@@ -11,7 +13,7 @@ const Stacked = ({width, height}) => {
     primaryYAxis={stackedPrimaryYAxis}
     chartArea={{border: {width:0}}}
     tooltip={{enable: true}}
-    legendSettings={{background: 'white'}}
+    background={currentMode === 'Dark' ? '#33373E' : '#fff'}
     >
       <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]}/>
       <SeriesCollectionDirective>

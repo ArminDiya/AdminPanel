67e73e1a4307de1ed7ca1006086c7158506db34d
ArminDiya <diyaarmin@gmail.com>
2022-08-27 18:18:14 +0430

    changing Pages Styles

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -5,22 +5,22 @@ import { Header } from '../component';
 
 const Orders = () => {
   return (
-    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
-    <Header category='Page' title='Orders'/>
-    <GridComponent
-    id='gridcomp'
-    dataSource={ordersData}
-    allowPaging
-    allowSorting
-    >
-      <ColumnsDirective>
-        {ordersGrid.map((item, index) => (
-          <ColumnDirective key={index} {...item}/>
-          
-        ))}
-      </ColumnsDirective>
-      <Inject services={[Resize, Sort, ContextMenu, Filter, Page, Edit, ExcelExport, PdfExport]}/>
-    </GridComponent>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Orders'/>
+      <GridComponent
+      id='gridcomp'
+      dataSource={ordersData}
+      allowPaging
+      allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item}/>
+            
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, Edit, ExcelExport, PdfExport]}/>
+      </GridComponent>
     </div>
   )
 }

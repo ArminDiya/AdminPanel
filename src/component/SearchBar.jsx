907c0111374e6da34ff0c75d788e5a2401504d82
ArminDiya <diyaarmin@gmail.com>
2022-09-03 18:55:23 +0430

    adding Search template App

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import { TextBox } from '@syncfusion/ej2-react-inputs'
-const SearchBar = () => {
-    
+import React from 'react';
+import { BiSearch } from 'react-icons/bi';
+import { useStateContext } from '../contexts/ContextProvider';
+const SearchBar = ({ placeholder, data }) => {
+  const {currentColor} = useStateContext();
+  console.log(currentColor);
   return (
-    <TextBox />
-  )
+<form>   
+    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+    <div className="relative">
+        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <div aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <BiSearch/>
+            </div>
+        </div>
+        <input type="search" id="default-search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required/>
+        <button style={{background: `${currentColor}`}} type="submit" className='text-white absolute right-2.5 bottom-2.5  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2'>Search</button>
+    </div>
+</form>
+  );
 }
-
+ 
 export default SearchBar
--- src/pages/Searches.jsx
+++ src/pages/Searches.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { Header } from '../component'
 import SearchBar from '../component/SearchBar';
 const Searches = () => {
-  htmlAttributes = { name: "username", type: "password", maxlength:"8" };
+
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
         <Header category="Pie" title="Project Cost Breakdown" />
         <div className="w-full">
-          <SearchBar htmlAttributes={htmlAttributes}/>
+          <SearchBar placeholder='Enter' />
         </div>
   </div>
   )
 }
 
-export default Searches
+export default Searches;
